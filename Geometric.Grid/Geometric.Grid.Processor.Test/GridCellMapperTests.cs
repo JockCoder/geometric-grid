da5b762d88769e34a51644da513f18db2e415c2a
Joe Jennings <joe.jennings@stoneridge.com>
2021-02-18 18:56:30 +0000

    Altered the GridCellMapper and removed the interface.  Added unit tests for the class.

---
+++ Geometric.Grid/Geometric.Grid.Processor.Test/GridCellMapperTests.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Geometric.Grid.Processor.Positioning;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometric.Grid.Processor.Test
+{
+    [TestClass()]
+    public class GridCellMapperTests
+    {
+        private GridCellMapper _gridCellMapper;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            _gridCellMapper = new GridCellMapper();
+        }
+
+        [TestMethod()]
+        public void TestValidGridNumericValuesFor_A_1()
+        {
+            _gridCellMapper.Row = "A";
+            _gridCellMapper.Column = 1;
+
+            Assert.AreEqual(1, _gridCellMapper.GetNumericRow());
+            Assert.AreEqual(1, _gridCellMapper.GetNumericColumn());
+        }
+
+        [TestMethod()]
+        public void TestValidGridNumericValuesFor_A_12()
+        {
+            _gridCellMapper.Row = "A";
+            _gridCellMapper.Column = 12;
+
+            Assert.AreEqual(1, _gridCellMapper.GetNumericRow());
+            Assert.AreEqual(12, _gridCellMapper.GetNumericColumn());
+        }
+
+        [TestMethod()]
+        public void TestValidGridNumericValuesFor_F_1()
+        {
+            _gridCellMapper.Row = "F";
+            _gridCellMapper.Column = 1;
+
+            Assert.AreEqual(6, _gridCellMapper.GetNumericRow());
+            Assert.AreEqual(1, _gridCellMapper.GetNumericColumn());
+        }
+
+        [TestMethod()]
+        public void TestValidGridNumericValuesFor_F_12()
+        {
+            _gridCellMapper.Row = "F";
+            _gridCellMapper.Column = 12;
+
+            Assert.AreEqual(6, _gridCellMapper.GetNumericRow());
+            Assert.AreEqual(12, _gridCellMapper.GetNumericColumn());
+        }
+
+
+        [TestMethod()]
+        public void TestValidGridNumericValuesFor_C_7()
+        {
+            _gridCellMapper.Row = "C";
+            _gridCellMapper.Column = 7;
+
+            Assert.AreEqual(3, _gridCellMapper.GetNumericRow());
+            Assert.AreEqual(7, _gridCellMapper.GetNumericColumn());
+        }
+
+        [TestMethod()]
+        public void TestInvalidGridNumericValuesFor_RowG()
+        {
+            _gridCellMapper.Row = "G";
+
+            int row;
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
+                row = _gridCellMapper.GetNumericRow()
+            );
+        }
+
+        [TestMethod()]
+        public void TestInvalidGridNumericValuesFor_Column13()
+        {
+            _gridCellMapper.Column = 13;
+
+            int column;
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
+                column = _gridCellMapper.GetNumericColumn()
+            );
+        }
+
+
+        [TestMethod()]
+        public void TestValidGridMappedValuesFor_A_1()
+        {
+            _gridCellMapper.SetGridMappedValues(1, 1);
+
+            Assert.AreEqual("A", _gridCellMapper.Row);
+            Assert.AreEqual(1, _gridCellMapper.Column);
+        }
+
+        [TestMethod()]
+        public void TestValidGridMappedValuesFor_A_12()
+        {
+            _gridCellMapper.SetGridMappedValues(1, 12);
+
+            Assert.AreEqual("A", _gridCellMapper.Row);
+            Assert.AreEqual(12, _gridCellMapper.Column);
+        }
+
+        [TestMethod()]
+        public void TestValidGridMappedValuesFor_F_1()
+        {
+            _gridCellMapper.SetGridMappedValues(6, 1);
+
+            Assert.AreEqual("F", _gridCellMapper.Row);
+            Assert.AreEqual(1, _gridCellMapper.Column);
+        }
+
+        [TestMethod()]
+        public void TestValidGridMappedValuesFor_F_12()
+        {
+            _gridCellMapper.SetGridMappedValues(6, 12);
+
+            Assert.AreEqual("F", _gridCellMapper.Row);
+            Assert.AreEqual(12, _gridCellMapper.Column);
+        }
+
+
+        [TestMethod()]
+        public void TestValidGridMappedValuesFor_C_7()
+        {
+            _gridCellMapper.SetGridMappedValues(3, 7);
+
+            Assert.AreEqual("C", _gridCellMapper.Row);
+            Assert.AreEqual(7, _gridCellMapper.Column);
+        }
+
+        [TestMethod()]
+        public void TestInvalidGridMappedValuesFor_G_1()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
+                _gridCellMapper.SetGridMappedValues(7, 1)
+            );
+        }
+
+        [TestMethod()]
+        public void TestInvalidGridMappedValuesFor_A_13()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
+                _gridCellMapper.SetGridMappedValues(1, 13)
+            );
+        }
+
+
+    }
+}
--- Geometric.Grid/Geometric.Grid.Processor/Positioning/GridCellMapper.cs
+++ Geometric.Grid/Geometric.Grid.Processor/Positioning/GridCellMapper.cs
@@ -6,10 +6,14 @@ using Geometric.Grid.Processor.Interfaces;
 
 namespace Geometric.Grid.Processor.Positioning
 {
-    public class GridCellMapper: IGridCellMapper
+    public class GridCellMapper
     {
         public string Row { get; set; }
         public int Column { get; set; }
+
+        private readonly string _alphaRow = "ABCDEF";
+        private const int MAX_COLUMN_LENGTH = 12;
+
 
         /// <summary>
         /// Use this to set to the mapping values
@@ -19,24 +23,30 @@ namespace Geometric.Grid.Processor.Positioning
         /// <param name="numericColumn"></param>
         public void SetGridMappedValues(int numericRow, int numericColumn)
         {
-            Column = numericColumn;
+            Column = setMappedColumn(numericColumn);
             Row = SetMappedRow(numericRow);
         }
 
         private string SetMappedRow(int numericRow)
         {
-            switch (numericRow)
+            if(numericRow > _alphaRow.Length)
             {
-                case 1: return "A";
-                case 2: return "B";
-                case 3: return "C";
-                case 4: return "D";
-                case 5: return "E";
-                case 6: return "F";
+                //we got here, so not a valid value
+                throw new ArgumentOutOfRangeException();
+            }
+            
+            //rem string chars are base 0, so take away 1.
+            return _alphaRow.Substring(numericRow - 1, 1);
+        }
+
+        private int setMappedColumn(int numericColumn)
+        {
+            if(numericColumn < 0 || numericColumn > MAX_COLUMN_LENGTH)
+            {
+                throw new ArgumentOutOfRangeException();
             }
 
-            //we got here, so not a valid value
-            throw new ArgumentOutOfRangeException();
+            return numericColumn;
         }
 
 
@@ -47,28 +57,32 @@ namespace Geometric.Grid.Processor.Positioning
         /// <returns></returns>
         public int GetNumericColumn()
         {
+            if (Column < 0 || Column > MAX_COLUMN_LENGTH)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
             return Column;
         }
 
         public int GetNumericRow()
         {
+            //We're only dealing with single chars for this grid mapping
             if (Row.Length > 1)
             {
                 throw new ArgumentOutOfRangeException();
             }
 
-            switch (Row)
+            int index = _alphaRow.IndexOf(Row);
+
+            //Didn't find it - throw our hands up
+            if(index == -1)
             {
-                case "A": return 1;
-                case "B": return 2;
-                case "C": return 3;
-                case "D": return 4;
-                case "E": return 5;
-                case "F": return 6;
+                throw new ArgumentOutOfRangeException();
             }
 
-            //we got here, so not a valid value
-            throw new ArgumentOutOfRangeException();
+            //Rem string is zero based, so add 1
+            return index + 1;
         }
     }
 }
